819f4bb2d91362aed997639fa18821edfadd97b2
Maram <mramalshbli@gmail.com>
2026-02-24 13:34:50 +0100

    removed unused component and fixed react import lint error

--- plugins/backstage-plugin-gamification/src/index.ts
+++ plugins/backstage-plugin-gamification/src/index.ts
@@ -8,5 +8,3 @@ export { rootRouteRef, xpRouteRef } from './routes';
 
 // valfritt, men bra att ha:
 export { XpDebugPage } from './components/XpDebugPage/XpDebugPage';
-
-export { XpLevelCard } from './components/XpLevelCard/XpLevelCard';
