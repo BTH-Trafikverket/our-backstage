fa27579e6da298f764eb142ebbb78006d57e9036
Kevin <kevin@gmail.com>
2026-02-24 14:51:51 +0100

    schema validation + completequest service

--- plugins/backstage-backend-gamification-backend/src/services/questsService.ts
+++ plugins/backstage-backend-gamification-backend/src/services/questsService.ts
@@ -41,4 +41,15 @@ export class QuestsService {
   async deleteQuest(id: string, _opts: QuestServiceOpts) {
     return this.questsRepo.deleteQuest(id);
   }
+
+  async completeQuest(
+    questId: string,
+    userRef: string,
+    _opts: QuestServiceOpts,
+  ) {
+    return this.questsRepo.completeQuestProgress({
+      quest_id: questId,
+      user_ref: userRef,
+    });
+  }
 }
